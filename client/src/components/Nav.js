7cac38a24993940e24b45da0687f84fa4186244b
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-07-16 23:56:20 -0500

    Updated Nav without bootstrap

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -5,13 +5,20 @@ import "../style/nav.css";
 
 function Nav() {
   return (
-    <nav class="navbar navbar-expand-md navbar-light navbar-default sticky-top">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="www.gayathri.dev">
+    <div class="navbar-container">
+      <div class="navbar-subcontainer">
+        <div class="left">
           <img class="logo" src={ImgLogo} alt="logo" />
-        </a>
+        </div>
+        <div class="right">
+          <div class="nav-items">Home</div>
+          <div class="nav-items">About</div>
+          <div class="nav-items">Portfolio</div>
+          <div class="nav-items">Blog</div>
+          <div class="nav-items">Contact</div>
+        </div>
       </div>
-    </nav>
+    </div>
   );
 }
 
