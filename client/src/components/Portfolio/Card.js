f634e900c060cd3c3a262c49c4f9c31c721c1027
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-07-22 17:57:56 -0500

    Refining the portfolio section

--- client/src/components/Portfolio/Card.js
+++ client/src/components/Portfolio/Card.js
@@ -1,11 +1,21 @@
 import React from "react";
+import ImgMenu from "../../assets/menu-more48.png";
 
 function Card(props) {
   return (
     <div className="card">
       <img src={props.card.img} alt="portfolio" />
       <div className="card-separator" />
-      <div className="card-txt">This is a sample text</div>
+      <div className="card-txt">
+        <div className="card-title">
+          <div>Project Name: </div>
+          <img className="card-img-more" src={ImgMenu} alt="More" />
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+          cumque tempore perspiciatis praesentium accusantium? Eum animi.
+        </p>
+      </div>
     </div>
   );
 }
