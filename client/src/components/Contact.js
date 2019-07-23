e1cee944b9e5b835792a0545edd352626315d12a
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-07-23 17:23:07 -0500

    Created the form

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -3,8 +3,38 @@ import "../style/contact.css";
 
 function Contact() {
   return (
-    <section className="contact-area" id="contact">
-      <h1>This is the Contact!!</h1>
+    <section className="contact-area">
+      <form>
+        <div className="contact-container">
+          <div className="contact-label">Full Name: </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Full name"
+              className="contact-input"
+            />
+          </div>
+          <div className="contact-label">Email address: </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Email address"
+              className="contact-input"
+            />
+          </div>
+          <div className="contact-label">Message: </div>
+          <div>
+            <textarea
+              name="message"
+              placeholder="Message"
+              className="contact-input-msg"
+            />
+          </div>
+          <div>
+            <button>Send Message</button>
+          </div>
+        </div>
+      </form>
     </section>
   );
 }
