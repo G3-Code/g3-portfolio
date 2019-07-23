b07e70a8be4762ba193fe4160ce06adfe96b28da
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-07-23 11:42:12 -0500

    Added transition for the image

--- client/src/components/Portfolio/Card.js
+++ client/src/components/Portfolio/Card.js
@@ -1,26 +1,40 @@
 import React from "react";
 import ImgMenu from "../../assets/menu-more48.png";
 
-function Card(props) {
-  return (
-    <div className="card">
-      <div className="card-front">
-        <img src={props.card.img} alt="portfolio" />
-        <div className="card-separator" />
-        <div className="card-txt">
-          <div className="card-title">
-            <div>Project Name: </div>
-            <img className="card-img-more" src={ImgMenu} alt="More" />
+class Card extends React.Component {
+  handleClick = e => {
+    console.log("handle clicked");
+    let card = document.querySelector(".card");
+    console.log(card);
+    card.style.transform = "rotateY(180deg)";
+  };
+  render() {
+    return (
+      <div className="card">
+        <div className="card-front">
+          <img src={this.props.card.img} alt="portfolio" />
+          <div className="card-separator" />
+          <div className="card-txt">
+            <div className="card-title">
+              <div>Project Name: </div>
+              <img
+                className="card-img-more"
+                src={ImgMenu}
+                alt="More"
+                onClick={this.handleClick}
+              />
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Temporibus cumque tempore perspiciatis praesentium accusantium?
+              Eum animi.
+            </p>
           </div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-            cumque tempore perspiciatis praesentium accusantium? Eum animi.
-          </p>
         </div>
+        <div className="card-back">This is the back of the card.</div>
       </div>
-      <div className="card-back">This is the back of the card.</div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Card;
