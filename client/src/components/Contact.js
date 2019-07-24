fcd57f315a726fc43c2be51a62933e117acdcba1
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-07-24 15:27:25 -0500

    Default form validation and responsiveness for contact page completed

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -7,39 +7,48 @@ class Contact extends React.Component {
       <section className="contact-area">
         <form name="contact" method="POST">
           <input type="hidden" name="form-name" value="contact" />
+
           <div className="contact-container">
-            <div className="contact-label">Full Name: </div>
-            <div>
-              <input
-                type="text"
-                placeholder="Full name"
-                className="contact-input"
-                name="name"
-              />
-            </div>
-            <div className="contact-label">Email address: </div>
-            <div>
-              <input
-                type="text"
-                placeholder="Email address"
-                className="contact-input"
-                name="email"
-              />
-            </div>
-            <div className="contact-label">Message: </div>
-            <div>
-              <textarea
-                name="message"
-                placeholder="Message"
-                className="contact-input-msg"
-              />
-            </div>
-            <div>
-              <input
-                type="submit"
-                className="contact-button"
-                value="Send Message"
-              />
+            <div className="contact-content-container">
+              <div className="contact-row">
+                <div className="contact-label">Full Name: </div>
+
+                <input
+                  type="text"
+                  placeholder="Full name"
+                  className="contact-input"
+                  name="name"
+                  required
+                />
+              </div>
+              <div className="contact-row">
+                <div className="contact-label">Email address: </div>
+
+                <input
+                  type="text"
+                  placeholder="Email address"
+                  className="contact-input"
+                  name="email"
+                  required
+                />
+              </div>
+              <div className="contact-row">
+                <div className="contact-label">Message: </div>
+
+                <textarea
+                  name="message"
+                  placeholder="Message"
+                  className="contact-input-msg"
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  type="submit"
+                  className="contact-button"
+                  value="Send Message"
+                />
+              </div>
             </div>
           </div>
         </form>
