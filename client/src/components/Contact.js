7270a050b3d2e6b3aa5aaa29074e6f104a532faa
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-07-24 10:22:16 -0500

    Testing netlify

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,48 +1,51 @@
 import React from "react";
 import "../style/contact.css";
 
-function Contact() {
-  return (
-    <section className="contact-area">
-      <form name="contact">
-        <div className="contact-container">
-          <div className="contact-label">Full Name: </div>
-          <div>
-            <input
-              type="text"
-              placeholder="Full name"
-              className="contact-input"
-              name="name"
-            />
+class Contact extends React.Component {
+  render() {
+    return (
+      <section className="contact-area">
+        <form name="contact" method="POST">
+          <input type="hidden" name="form-name" value="contact" />
+          <div className="contact-container">
+            <div className="contact-label">Full Name: </div>
+            <div>
+              <input
+                type="text"
+                placeholder="Full name"
+                className="contact-input"
+                name="name"
+              />
+            </div>
+            <div className="contact-label">Email address: </div>
+            <div>
+              <input
+                type="text"
+                placeholder="Email address"
+                className="contact-input"
+                name="email"
+              />
+            </div>
+            <div className="contact-label">Message: </div>
+            <div>
+              <textarea
+                name="message"
+                placeholder="Message"
+                className="contact-input-msg"
+              />
+            </div>
+            <div>
+              <input
+                type="submit"
+                className="contact-button"
+                value="Send Message"
+              />
+            </div>
           </div>
-          <div className="contact-label">Email address: </div>
-          <div>
-            <input
-              type="text"
-              placeholder="Email address"
-              className="contact-input"
-              name="email"
-            />
-          </div>
-          <div className="contact-label">Message: </div>
-          <div>
-            <textarea
-              name="message"
-              placeholder="Message"
-              className="contact-input-msg"
-            />
-          </div>
-          <div>
-            <input
-              type="submit"
-              className="contact-button"
-              value="Send Message"
-            />
-          </div>
-        </div>
-      </form>
-    </section>
-  );
+        </form>
+      </section>
+    );
+  }
 }
 
 export default Contact;
