41d45006ac07a6869d65276f15d00341aebb2b20
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-07-30 18:47:15 -0500

    Added the justhelp screenshot

--- client/src/components/Portfolio/data.js
+++ client/src/components/Portfolio/data.js
@@ -1,4 +1,11 @@
-export const tabData = ["all", "javascript", "node", "React.js", "HTML5/CSS3"];
+export const tabData = [
+  "all",
+  "javascript",
+  "node",
+  "React.js",
+  "HTML5/CSS3",
+  "SQL"
+];
 
 export const cardData = [
   {
@@ -21,108 +28,15 @@ export const cardData = [
   },
   {
     id: "2",
-    projectName: "Project 2",
-    description:
-      "Type Coercion: Why Does NaN !== NaN? and Other Strange Occurences",
-    tab: ["boostrap"],
-    img: "./assets/test.jpg",
+    projectName: "Random act generator",
+    description: "A web app that suggests a random act of kindness.",
+    tab: ["javascript", "React.js", "node", "SQL", "HTML5/CSS3"],
+    img: "./assets/justhelp.png",
     accomplishment: [
       { id: "a21", desc: "Accomplishment1" },
       { id: "a22", desc: "Accomplishment2" },
       { id: "a23", desc: "Accomplishment3" },
       { id: "a24", desc: "Accomplishment4" }
-    ],
-    github: "www.github.com",
-    online: "www.google.com"
-  },
-  {
-    id: "3",
-    projectName: "Project 3",
-    description: "AI: What Are the Ethical Rammifications of the Future?",
-    tab: ["technology"],
-    img: "./assets/test.jpg",
-    accomplishment: [
-      { id: "a31", desc: "Accomplishment1" },
-      { id: "a32", desc: "Accomplishment2" },
-      { id: "a33", desc: "Accomplishment3" },
-      { id: "a34", desc: "Accomplishment4" }
-    ],
-    github: "www.github.com",
-    online: "www.google.com"
-  },
-  {
-    id: "4",
-    projectName: "Project 4",
-    description: "History of Javascript Libraries: from jQuery to React",
-    tab: ["React.js"],
-    img: "./assets/test.jpg",
-    accomplishment: [
-      { id: "a41", desc: "Accomplishment1" },
-      { id: "a42", desc: "Accomplishment2" },
-      { id: "a43", desc: "Accomplishment3" },
-      { id: "a44", desc: "Accomplishment4" }
-    ],
-    github: "www.github.com",
-    online: "www.google.com"
-  },
-  {
-    id: "5",
-    projectName: "Project 5",
-    description: "Node.js in 2018: Which Framework? Express, Sails, Hapi?",
-    tab: ["node"],
-    img: "./assets/test.jpg",
-    accomplishment: [
-      { id: "a51", desc: "Accomplishment1" },
-      { id: "a52", desc: "Accomplishment2" },
-      { id: "a53", desc: "Accomplishment3" },
-      { id: "a54", desc: "Accomplishment4" }
-    ],
-    github: "www.github.com",
-    online: "www.google.com"
-  },
-  {
-    id: "6",
-    projectName: "Project 6",
-    description: "MongoDB: NoSQL vs. SQL, the Debate Continues...",
-    tab: ["node"],
-    img: "./assets/test.jpg",
-    accomplishment: [
-      { id: "a61", desc: "Accomplishment1" },
-      { id: "a62", desc: "Accomplishment2" },
-      { id: "a63", desc: "Accomplishment3" },
-      { id: "a64", desc: "Accomplishment4" }
-    ],
-    github: "www.github.com",
-    online: "www.google.com"
-  },
-  {
-    id: "7",
-    projectName: "Project 7",
-    description:
-      "When to Rest, When to Spread: Why There Are Two Meanings Behind '...'",
-    tab: ["javascript"],
-    img: "./assets/test.jpg",
-    accomplishment: [
-      { id: "a71", desc: "Accomplishment1" },
-      { id: "a72", desc: "Accomplishment2" },
-      { id: "a73", desc: "Accomplishment3" },
-      { id: "a74", desc: "Accomplishment4" }
-    ],
-    github: "www.github.com",
-    online: "www.google.com"
-  },
-  {
-    id: "8",
-    projectName: "Project 8",
-    description:
-      "The Hottest New Bootstrap 4 Components Every Developer Needs to Know About",
-    tab: ["bootstrap"],
-    img: "./assets/test.jpg",
-    accomplishment: [
-      { id: "a81", desc: "Accomplishment1" },
-      { id: "a82", desc: "Accomplishment2" },
-      { id: "a83", desc: "Accomplishment3" },
-      { id: "a84", desc: "Accomplishment4" }
     ],
     github: "www.github.com",
     online: "www.google.com"
